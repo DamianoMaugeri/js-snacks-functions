59bea9253859c4a306fb80fe13a8677c3ce5a854
DamianoMaugeri <damianomaugeri97@icloud.com>
2024-10-21 16:02:44 +0200

    [add] solution 2

--- snack-funzioni/02 - Filtra iniziali/script.js
+++ snack-funzioni/02 - Filtra iniziali/script.js
@@ -4,10 +4,22 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+const startwith = (array , a) => {
+    const result = []
+    for (let i=0; i<array.length; i++){
+        let word = array[i]
+        if (word[0]===a) {
+            result.push(word)
+        }
+        
+    }
+    return result
+}
+
 
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(startwith(names, 'A'))
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
