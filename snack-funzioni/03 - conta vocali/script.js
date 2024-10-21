4add8b0e5446fc6df97e22448aeb3e261d8d4e1f
DamianoMaugeri <damianomaugeri97@icloud.com>
2024-10-21 16:20:51 +0200

    [add] 03 solution

--- snack-funzioni/03 - conta vocali/script.js
+++ snack-funzioni/03 - conta vocali/script.js
@@ -2,12 +2,24 @@
 
 const word = 'javascript';
 
+const lcWord = word.toLocaleLowerCase()
+
 
 // Dichiara la funzione qui.
+const numberOfVocal = (string) => {
+    let vocal=0
+    for (let i=0; i<string.length; i++){
+        if((string[i]==='a') || (string[i]==='e') || (string[i]==='i') || (string[i]==='o') || (string[i]==='u')  /* || (string[i]==='y')  ?? */ ){
+            vocal += 1
+        }
+    }
+    return vocal
+
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(numberOfVocal(lcWord))
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
